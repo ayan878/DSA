bb0c573297a39a03646e3a8a12fdf52d14536723
ayan878 <ayanraza197@gmail.com>
2024-07-11 22:10:03 +0530

    Palindrome

--- JavaScript/Basic Maths/Palindrome.js
+++ JavaScript/Basic Maths/Palindrome.js
@@ -1,14 +1,19 @@
-function palindrome(n) {
-    let original = n;
-    let rev = 0;
-    let rem = 0;
+class Palindrome {
+     isPalindrome(n) {
+        let original = n;
+        let rev = 0;
+        let rem = 0;
 
-    while (n > 0) {
-        rem = n % 10;
-        rev = rev * 10 + rem;
-        n = Math.floor(n / 10);
+        while (n > 0) {
+            rem = n % 10;
+            rev = rev * 10 + rem;
+            n = Math.floor(n / 10);
+        }
+
+        return rev === original;
     }
-    return rev === original;
 }
-console.log(palindrome(121));
 
+const palindrome = new Palindrome();
+console.log(palindrome.isPalindrome(121)); // Output: true
+console.log(palindrome.isPalindrome(123)); // Output: false
