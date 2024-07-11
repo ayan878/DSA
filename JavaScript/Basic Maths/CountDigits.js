3aa02d75104c66de1f7f9a248a8e248aa076d29c
ayan878 <ayanraza197@gmail.com>
2024-07-11 21:57:03 +0530

    count digits

---
+++ JavaScript/Basic Maths/CountDigits.js
@@ -0,0 +1,11 @@
+function countDigits(n) {
+    let count = 0;
+
+    while (n > 0) {
+        count++;
+        n = Math.floor(n / 10);
+    }
+
+    return count;
+}
+console.log(countDigits(325));
