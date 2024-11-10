9980eecd56562087821d653e9206861a8129c5da
ayan878 <ayanraza197@gmail.com>
2024-11-11 02:41:51 +0530

    dynamic programming

---
+++ JavaScript/Dynamic Programming/Fibonnaci/fibonacciTabulation.js
@@ -0,0 +1,11 @@
+function fib(n){
+    let dp=[n+1];
+    dp[0]=0;
+    dp[1]=1;
+
+    for (let i = 2; i<=n ; i++) {
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+console.log(fib(6))
